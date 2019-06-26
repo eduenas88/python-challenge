8b04ca187a9ee3b8107d87e4eb23fe81467741b5
Elizabeth Duenas <salgadoe88@gmail.com>
2019-06-26 12:05:13 -0500

    Update main.py
    playing with the code

---
+++ PyBank/main.py
@@ -0,0 +1,37 @@
+import os
+import csv
+
+budget_data_csv_path = os.path.join("Pybank","budget_data.csv")
+
+# Open and read csv
+with open(budget_data_csv_path, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first 
+    csv_header = next(csvfile)
+    
+    #Set variables to zero 
+    month_count = 0
+    money = 0
+    average = 0
+
+# defining the main function for my calculations
+def print_percentages(budget_data):
+    # For readability, it can help to assign your values to variables with descriptive names
+    #date = str(budget_data[0])
+    total = int(budget_data[1])
+    #averagechange = int(wrestler_data[2])
+    #averageincrease = int(wrestler_data[3])
+    
+    for row in csv_reader:
+        month_count+=1
+        
+
+    print(f"Financial Analysis")
+    print(f"--------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Total : {total}")
+    print(f"Average Change : {total/month_count}")
+    #print(f"Greatest Increase in Profit : {total/month_count}")
+    #print(f"Greatest Decrease in Profit : {total/month_count}")
+ 
