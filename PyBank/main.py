8757db7f3bfe618b4ae05398114d7b480ce52c2a
Elizabeth Duenas <salgadoe88@gmail.com>
2019-06-26 21:40:05 -0500

    Working on HW
    working on hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,15 @@
 import os
 import csv
 
+# defining the main function for my calculations
+def print_percentages(budget_data_csv_path):
+    # For readability, it can help to assign your values to variables with descriptive names
+    #date = str(budget_data[0])
+    # total = int(budget_data[1])
+    #averagechange = int(wrestler_data[2])
+    #averageincrease = int(wrestler_data[3])
+    pass
+    
 budget_data_csv_path = os.path.join("Pybank","budget_data.csv")
 
 # Open and read csv
@@ -15,23 +24,19 @@ with open(budget_data_csv_path, newline='') as csvfile:
     money = 0
     average = 0
 
-# defining the main function for my calculations
-def print_percentages(budget_data):
-    # For readability, it can help to assign your values to variables with descriptive names
-    #date = str(budget_data[0])
-    total = int(budget_data[1])
-    #averagechange = int(wrestler_data[2])
-    #averageincrease = int(wrestler_data[3])
-    
+    from statistics import mean
+    mean= (budget_data_csv_path[1])
+    print(mean)
+
     for row in csv_reader:
         month_count+=1
-        
+            
 
-    print(f"Financial Analysis")
-    print(f"--------------------")
-    print(f"Total Months: {month_count}")
-    print(f"Total : {total}")
-    print(f"Average Change : {total/month_count}")
-    #print(f"Greatest Increase in Profit : {total/month_count}")
-    #print(f"Greatest Decrease in Profit : {total/month_count}")
+        print(f"Financial Analysis")
+        print(f"--------------------")
+        print(f"Total Months: {month_count}")
+        print(f"Total : {total}")
+        print(f"Average Change : {total/month_count}")
+        #print(f"Greatest Increase in Profit : {total/month_count}")
+        #print(f"Greatest Decrease in Profit : {total/month_count}")
  
