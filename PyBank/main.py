70225356be146c1c027bcdcb0ef4ef3c1357cb32
Elizabeth Duenas <salgadoe88@gmail.com>
2019-06-28 17:20:52 -0500

    working on hw some more
    working on hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,15 @@
 import os
 import csv
 
-# defining the main function for my calculations
-def print_percentages(budget_data_csv_path):
-    # For readability, it can help to assign your values to variables with descriptive names
-    #date = str(budget_data[0])
-    # total = int(budget_data[1])
-    #averagechange = int(wrestler_data[2])
-    #averageincrease = int(wrestler_data[3])
-    pass
-    
+dates= []
+profit_losses= []
+
 budget_data_csv_path = os.path.join("Pybank","budget_data.csv")
 
 # Open and read csv
 with open(budget_data_csv_path, newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-
+    
     # Read the header row first 
     csv_header = next(csvfile)
     
@@ -24,19 +18,39 @@ with open(budget_data_csv_path, newline='') as csvfile:
     money = 0
     average = 0
 
-    from statistics import mean
-    mean= (budget_data_csv_path[1])
-    print(mean)
+    min = 0
+    max = 0
+
+    counter = 0
+
 
     for row in csv_reader:
-        month_count+=1
-            
+        dates.append(row[0])
+        profit_losses.append(int(row[1]))
 
-        print(f"Financial Analysis")
-        print(f"--------------------")
-        print(f"Total Months: {month_count}")
-        print(f"Total : {total}")
-        print(f"Average Change : {total/month_count}")
-        #print(f"Greatest Increase in Profit : {total/month_count}")
-        #print(f"Greatest Decrease in Profit : {total/month_count}")
+        month_count+=1
+        money+=int(row[1]) 
+
+        if abs(profit_losses[counter]-(profit_losses[counter-1])) > min:
+            min=abs(profit_losses[counter-1]-int(row[1]))
+            min_month=row[0]
+        if (profit_losses[counter]-(profit_losses[counter-1])) > max:
+            max=(profit_losses[counter-1]-int(row[1]))
+            max_month=row[0]
+        
+        counter+=1
+
+
+
+
+           
+
+    print("Financial Analysis")
+    print(f"--------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Total : ${money}")
+    #print(f"Average Change : {money/month_count}")
+    print(f"Greatest Increase in Profit : {max_month} {max}")
+    print(f"Greatest Decrease in Profit : {min_month} ${min*-1}")
+
  
